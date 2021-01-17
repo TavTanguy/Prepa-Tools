b9567a18c514e64946543b6e54cbe28adc551ec0
Taverna Tanguy <taverna.tanguy@gmail.com>
2021-01-17 22:08:27 +0100

    Fix register sw

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -4,18 +4,6 @@ import { register } from "register-service-worker";
 
 if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
-    },
-    registered() {
-    },
-    cached() {
-    },
-    updatefound() {
-    },
-    updated() {
-    },
-    offline() {
-    },
     error(error) {
       console.error("Error during service worker registration:", error);
     }
