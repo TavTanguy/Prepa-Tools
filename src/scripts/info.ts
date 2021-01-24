c4fbca001d8b6756a00536eccf3af91ff5746c14
Taverna Tanguy <taverna.tanguy@gmail.com>
2021-01-24 19:23:15 +0100

    Add voc

--- src/scripts/info.ts
+++ src/scripts/info.ts
@@ -1,3 +1,4 @@
 export const tools = [
-  {name: "Compteur de mots", icon: "pencil.svg", path: "/cmtMots", component: () => import(/* webpackChunkName: "cmtMots" */"../views/CmtMots.vue")}
+  {name: "Compteur de mots", icon: "pencil.svg", path: "/cmtMots", component: () => import(/* webpackChunkName: "cmtMots" */"../views/CmtMots.vue")},
+  {name: "Vocabulaire", icon: "pencil.svg", path: "/voc", component: () => import(/* webpackChunkName: "cmtMots" */"../views/Voc.vue")}
 ]
--- src/views/Voc.vue
+++ src/views/Voc.vue
@@ -1,82 +1,174 @@
 <template>
-  <div class="text-center">
-    <h1 class="text-h2">Entrainement vocabulaire</h1>
-    <v-textarea
-          v-model="words"
-          name="input-7-1"
-          label="Votre liste de mots séparé par / et ; entre les deux langues (ex: Hello;Bonjour / World;Monde)"
-          value=""
-          outlined
-        ></v-textarea>
-        <v-btn @click="lunch">Lancer</v-btn>
-        <v-form class="d-flex">
-          <p class="text-body-1">{{wordGess}}</p>
-          <v-text-field outlined :error="invalidWord" v-model="write"/>
-          <v-text-field outlined :value="showSolution? solution : ''" disabled/>
-          <v-btn color="primary" @keydown="showTheSolution" @click="showTheSolution">Montrer</v-btn>
-          <v-btn color="primary" @keydown.arrow-right="nextWord" @click="nextWord">Suivant</v-btn>
-        </v-form>
+  <div class="voc">
+    <v-dialog v-model="showDialog">
+      <v-card>
+        <v-card-title>
+        Ajouter des mots
+        </v-card-title>
+        <v-divider class="my-4"/>
+        <v-card-text>
+           <v-combobox
+              v-model="inputWords"
+              hide-selected
+              multiple
+              small-chips
+
+              flat
+              background-color="light"
+              solo
+              deletable-chips
+              placeholder="ex: Français:French"
+              label="Vos mots"
+            />
+          <label for="random">Ordre aléatoire de la liste</label>
+          <v-switch v-model="randomOrder" name="random" class="d-inline-block ml-3 mr-1"/>
+
+          <label for="randomDirection">Langue aléatoire de la liste</label>
+          <v-switch v-model="randomDirection" name="randomDirection" class="d-inline-block ml-3 mr-1"/>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn color="light" @click="showDialog = false">Annuler</v-btn>
+          <v-btn color="primary" x-large @click="saveWords">Valider</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-btn elevation="2" color="primary" style="bottom: 12px" fab large absolute bottom right @click="showDialog = true"><v-icon large>add</v-icon></v-btn>
+    <title-under class="mt-5">Vocabulaire</title-under>
+    <v-progress-linear class="mx-10 mt-8" color="light" :value="((showRes?currentIndex+1:currentIndex)/listWords.length) * 100" />
+    <div class="mx-10 mt-8 d-flex justify-center">
+      <v-btn class="mt-2 mr-10" color="light" text @click="start">Recommencer</v-btn>
+      <v-badge
+        class="d-inline-block"
+        color="primary"
+        :content="ask"
+        left
+      >
+        <v-text-field solo v-model="input" @keydown="keyDown"/>
+      </v-badge>
+      <v-btn class="mt-2 ml-7" color="primary" @click="showRes=true">Valider</v-btn>
+      <v-btn class="mt-2 ml-7" color="light" @click="next">Suivant</v-btn>
+    </div>
+    <p class="text-center" :class="colorText !== ''? colorText+'--text':''" v-show="showRes">Réponse: {{res}}</p>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue"
-export default Vue.extend({
-  data: ()=>({
-    words: "",
-    listWordsGess: [] as Array<string>,
-    indexWord: 0,
-    wordGess: "",
-    solution: "",
-    showSolution: false,
-    write: "",
-    invalidWord: false
-  }),
-  mounted(){
-    this.words = localStorage.getItem("voc") ?? ""
-  },
-  methods: {
-    randomize(tab: Array<any>) {
-      let i, j, tmp;
-      for (i = tab.length - 1; i > 0; i--) {
-          j = Math.floor(Math.random() * (i + 1));
-          tmp = tab[i];
-          tab[i] = tab[j];
-          tab[j] = tmp;
-      }
-      return tab;
-    },
-    getRandomIntInclusive(min: number, max: number) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min +1)) + min;
-    },
-    lunch(){
-      const couples = this.words.split("/")
-      this.listWordsGess = this.randomize(couples)
-      this.indexWord = 0
-      this.nextWord();
-      localStorage.setItem("voc", this.words)
-    },
-    nextWord(){
-      this.indexWord += 1;
-      this.showSolution = false
-      this.invalidWord = false
-      if(this.listWordsGess.length <= this.indexWord)
-        this.indexWord = 0
+import Vue from "vue";
+import TitleUnder from "../components/Title.vue";
 
-      const couple = this.listWordsGess[this.indexWord].split(";")
-      const i = this.getRandomIntInclusive(0, 1);
-      this.wordGess = couple[i];
-      this.solution = couple[(i + 1) % 2];
-      this.write = ""
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+export default Vue.extend({
+  components: {TitleUnder},
+  data: ()=>({
+    showDialog: false,
+    inputWords: [] as string[],
+    listWordsSelect: [] as string[][],
+    input: "",
+    currentIndex: 0,
+    listWords: [] as string[][],
+    showRes: false as boolean,
+    randomOrder: false,
+    randomDirection: false
+  }),
+  methods: {
+    keyDown(ev: KeyboardEvent){
+      switch (ev.code) {
+        case "Enter":
+          this.validWord()
+          break;
+        case "ArrowRight":
+          this.next()
+          break
+      }
     },
-    showTheSolution(){
-      this.showSolution = true;
+    validWord(){
+      if(!this.showRes)
+        this.showRes = true;
+      else
+        this.next()
+    },
+    saveWords(){
+      const words: string[][] = []
+      this.inputWords.forEach(el=>{
+        const l = el.split(":")
+        if(l.length !== 2) return;
+        words.push([...l])
+      });
+      localStorage.setItem("vocWords", JSON.stringify(words));
+      this.listWordsSelect = words;
+      this.showDialog = false;
+      this.start();
+    },
+    start(){
+      this.currentIndex = 0;
       
-      if(this.write.toLowerCase().trim() != this.solution.toLowerCase().trim())
-        this.invalidWord = true
+      let worlds = this.listWordsSelect;
+      if(this.randomOrder){
+        const worldRemaining = Array.from(worlds)
+        worlds = []
+        while(worldRemaining.length > 0){
+          const i = getRandomInt(0, worldRemaining.length);
+          worlds.push(worldRemaining[i])
+          worldRemaining.splice(i, 1)
+        }
+      }
+      if(this.randomDirection)
+        worlds = worlds.map((el)=>Math.random()>0.5?el:[el[1], el[0]]);
+      
+
+      this.listWords = worlds;
+      this.input = "";
+      this.showRes = false
+    },
+    next(){
+      this.input = "";
+      this.showRes = false;
+      const newIndex = this.currentIndex+1;
+      if(newIndex >= this.listWords.length)
+        this.start()
+      else
+        this.currentIndex = newIndex
+    }
+  },
+  computed: {
+    res(){
+      const currentWord = this.listWords[this.currentIndex]
+      return currentWord? currentWord[1] : ""
+    },
+    ask(){
+      const currentWord = this.listWords[this.currentIndex]
+      return currentWord ? currentWord[0] : ""
+    },
+    colorText(){
+      if(this.input.toLowerCase() == this.res.toLowerCase())
+        return "primary"
+      if(this.input == "")
+        return ""
+      return "red"
+    }
+  },
+  mounted(){
+    const words = localStorage.getItem("vocWords");
+    if(!words) this.showDialog = true;
+    else{
+      const listWords: string[][] = JSON.parse(words)
+      this.inputWords = listWords.map(el=>el.join(":"));
+      this.listWordsSelect = listWords;
+      this.start()
     }
   }
 })
 </script>
+
+<style scoped>
+.voc {
+  width: 95vw;
+  margin: auto;
+}
+</style>
